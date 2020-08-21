0f8f024cf6d753ad4238064219ccd8eea03f26d0
SSDesth <carlosmontt19@hotmail.com>
2020-08-20 21:21:12 -0600

    recpositorio del Api proyecto final

---
+++ controllers/register.js
@@ -0,0 +1,35 @@
+
+const hanbleregister = (req, res,db, bcrypt, saltRounds) =>{
+	
+	const {email, password, name} = req.body;
+	
+	if(!email || !name || !password){
+		return res.status(400).json('Incorrect form')
+	}
+	const hash = bcrypt.hashSync(password, saltRounds);
+	
+	db.transaction(trx =>{
+		
+		trx.insert({
+			hash : hash,
+			email:email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail =>{
+			trx('users').returning('*')
+			.insert({
+				email:loginEmail[0],
+				name:name,
+				joined:new Date()
+			}).then(user=>{
+				res.json(user[0]);
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		}).catch(err=>res.status(400).json('Unable to Register'));
+	})
+}
+module.exports={
+	hanbleregister:hanbleregister
+};
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '115410802',
+    database : 'smart-brain'
+  }
+});
+
+const app =  express();
+
+const saltRounds = 10;
+
+app.use(express.json());
+app.use(cors());
+
+/* ======== iniciar el server ========*/
+	app.get('/',(req,res) =>{
+		res.send(database.users);
+	})
+
+
+	app.listen(3000, ()=>{
+		console.log('app is running on port 3000');
+	});
+
+/* ======== Signin ======== */
+	app.post('/signin', (req,res)=>{
+		signin.hanbleSignin(req,res, db, bcrypt,saltRounds)
+	});
+
+/* ======== Register ======== */
+	app.post('/register',(req,res)=>{
+		register.hanbleregister(req,res, db, bcrypt,saltRounds)
+	});
+
+/* ======== Profile ======== */
+	app.get('/profile/:id',(req,res)=>{
+		profile.hanbleProfile(req,res,db)
+	});
+
+/* ======== Image ======== */
+	app.put('/image' , (req,res)=>{
+		image.hanbleImage(req,res,db)
+	});
+
+/* ======== Api ======== */
+app.post('/imageurl' , (req,res)=>{
+	console.log('test')
+		image.handleApiCall(req,res)
+	});
