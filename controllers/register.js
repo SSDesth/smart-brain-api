4e0402f097ebd8f0c7662456bcf98044fb8e6679
SSDesth <carlosmontt19@hotmail.com>
2020-08-21 01:48:32 -0600

    database update

--- controllers/register.js
+++ controllers/register.js
@@ -6,30 +6,30 @@ const hanbleregister = (req, res,db, bcrypt) =>{
 	if(!email || !name || !password){
 		return res.status(400).json('Incorrect form')
 	}
-	res.status(400).json("email:",email, "pass:",password, "Name:",name)
 	const hash = bcrypt.hashSync(password);
-	
-	db.transaction(trx =>{
-		
-		trx.insert({
-			hash : hash,
-			email:email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail =>{
-			trx('users').returning('*')
-			.insert({
-				email:loginEmail[0],
-				name:name,
-				joined:new Date()
-			}).then(user=>{
-				res.json(user[0]);
-			})
-			.then(trx.commit)
-			.catch(err=>res.status(400).json('Unable to Register!'));
-		}).catch(err=>res.status(400).json('Unable to Register'));
-	}).catch(err=>res.status(400).json('UUn error'));
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 }
 module.exports={
 	hanbleregister:hanbleregister
